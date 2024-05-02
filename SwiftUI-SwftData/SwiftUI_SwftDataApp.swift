70c8409cd5ddb704ee9c971ed2cac62a396dc4d2
Zakaria Lachkar <lachkar99zakaria@gmail.com>
2024-05-02 15:38:26 +0100

    init

---
+++ SwiftUI-SwftData/SwiftUI_SwftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SwftDataApp.swift
+//  SwiftUI-SwftData
+//
+//  Created by Zakaria Lachkar on 2/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SwftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
